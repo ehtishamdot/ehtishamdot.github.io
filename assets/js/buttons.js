507c93ff1e9c6520c6c030fdd07781b18b30a5a9
Ehtisham Ul Haq <ahtionpc1@gmail.com>
2021-08-12 00:44:49 +0500

    theme updation

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,6 +1,29 @@
-const primaryBtn = document.querySelector('.primary-button');
-const btnCursor = document.querySelector(".iconify");
 
-primaryBtn.addEventListener('mouseover',function(){
-    btnCursor.style.transform = 'rotateY'
-})
+const blackWhiteTheme = document.querySelector(".black-white-btn");
+const defaultTheme = document.querySelector(".default-btn");
+const greenTheme = document.querySelector(".green-btn");
+
+blackWhiteTheme.addEventListener('click', function () {
+    document.documentElement.style.setProperty("--main-bg-color", "black");
+    document.documentElement.style.setProperty(
+      "--primary-main-color",
+      "#32c7cf"
+    );
+    document.documentElement.style.setProperty("--primary-sec-color", "#fff");
+    document.documentElement.style.setProperty("--important-color", "#fff");
+});
+
+defaultTheme.addEventListener("click", function () {
+  document.documentElement.style.setProperty("--main-bg-color", "#0c1c27");
+  document.documentElement.style.setProperty("--primary-main-color", "#32c7cf");
+  document.documentElement.style.setProperty("--primary-sec-color", "#9cc8e5");
+  document.documentElement.style.setProperty("--important-color", "#abdbf8");
+}); 
+
+greenTheme.addEventListener("click", function () {
+  document.documentElement.style.setProperty("--main-bg-color", "#0a192f");
+  document.documentElement.style.setProperty("--primary-main-color", "#32c7cf");
+  document.documentElement.style.setProperty("--primary-sec-color", "#ccd6f6");
+  document.documentElement.style.setProperty("--important-color", "#64ffda");
+});
+
