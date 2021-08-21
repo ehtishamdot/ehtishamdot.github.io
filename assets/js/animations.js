6ba7bedb7a858c62454b3942739d71cc69ef7d39
Ehtisham Ul Haq <ahtionpc1@gmail.com>
2021-08-21 17:20:56 +0500

    Update animations.js

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -43,7 +43,7 @@ const inputAboutMe = function (entries) {
 
 const observeAboutMe = new IntersectionObserver(inputAboutMe, {
   root: null,
-  threshold: 0.1,
+  threshold: 0.2,
 });
 
 observeAboutMe.observe(aboutMe);
