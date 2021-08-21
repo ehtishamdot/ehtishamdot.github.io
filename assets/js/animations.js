7de492cc58eab5421ad3576b0b97b01ad839de37
Ehtisham Ul Haq <ahtionpc1@gmail.com>
2021-08-21 20:04:07 +0500

    Update animations.js

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,49 +1,80 @@
 //variables
 const aboutMe = document.querySelector(".about-me");
 const boldImp = document.querySelectorAll(".bold-imp");
+const topImg = document.querySelector(".top-img");
+
 const aboutMeMdi = document.querySelectorAll(".mdi");
+const aboutMeWork = document.querySelector(".about-me__container__work");
 const aboutMePara = document.querySelector(".about-visual__intro .col1");
 const aboutMeImage = document.querySelector(".about-visual__intro-image");
-const topImg = document.querySelector(".top-img"); 
+
+//variables
+
+//Animation adder
+const animationAdder = function (element, value) {
+  element.style.animation = value;
+};
+//Class adders
+const classAdder = function (element, value) {
+  element.classList.add(value);
+};
+//Class remover
+const classRemover = function (element, value) {
+  element.classList.remove(value);
+};
+
 
 
 const defaults = function () {
   boldImp.forEach((op) => {
-    op.classList.add("opacity-zero");
+    classAdder(op, "opacity-zero");
   });
   aboutMeMdi.forEach((mdi) => {
-    mdi.classList.add("opacity-zero");
+    classAdder(mdi, "opacity-zero");
   });
-  aboutMePara.classList.add("opacity-zero");
-  aboutMeImage.classList.add("opacity-zero");
+  classAdder(aboutMePara, "opacity-zero");
+  classAdder(aboutMeImage, "opacity-zero");
 };
-
 defaults();
 
+
 const inputAboutMe = function (entries) {
+
   const [entry] = entries;
-  console.log(entry.isIntersecting);
+
   if (entry.isIntersecting) {
     boldImp.forEach((op) => {
-      op.style.animation = "opacity 1.5s ease-in-out";
-      op.classList.remove("opacity-zero");
-    });
-    aboutMeMdi.forEach((mdi) => {
-      mdi.classList.remove("opacity-zero");
-      mdi.style.animation = "sideUp 1.5s both";
-    });
-    aboutMePara.classList.remove("opacity-zero");
-    aboutMeImage.classList.remove("opacity-zero");
-    aboutMePara.style.animation = "moveRight 1s ease-in-out backwards";
-    aboutMeImage.style.animation = "moveRightLeft 1s ease-in-out  backwards";
 
+      animationAdder(op, "opacity 1.5s ease-in-out");
+      classRemover(op, "opacity-zero");
+    });
 
-}
+    classRemover(aboutMePara, "opacity-zero");
+    classRemover(aboutMeImage, "opacity-zero");
+    animationAdder(aboutMePara, "moveRight 1s ease-in-out backwards");
+    animationAdder(aboutMeImage, "moveRightLeft 1s ease-in-out  backwards");
+  }
 };
 
 const observeAboutMe = new IntersectionObserver(inputAboutMe, {
   root: null,
-  threshold: 0.1,
+  threshold: 0.5,
 });
-
 observeAboutMe.observe(aboutMe);
+
+const inputMdi = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (entry.isIntersecting) {
+    aboutMeMdi.forEach((mdi) => {
+      animationAdder(mdi, "sideUp 1.5s both");
+      mdi.classList.remove("opacity-zero");
+    });
+  }
+};
+
+const observeMdi = new IntersectionObserver(inputMdi, {
+  root: null,
+  threshold: 0.44,
+});
+observeMdi.observe(aboutMeWork);
