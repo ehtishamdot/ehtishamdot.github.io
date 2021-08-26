27f19d1a9dd65e0d7d99030150584da277f839b1
Ehtisham Ul Haq <ahtionpc1@gmail.com>
2021-08-26 19:43:00 +0500

    animation added again

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -5,6 +5,7 @@ const topImg = document.querySelector(".top-img");
 
 const aboutMeMdi = document.querySelectorAll(".work-logo");
 const aboutMeWork = document.querySelector(".about-me__container__work");
+const aboutMeVisual = document.querySelector(".about-visual");
 const aboutMePara = document.querySelector(".about-visual__intro .col1");
 const aboutMeImage = document.querySelector(".about-visual__intro-image");
 
@@ -23,44 +24,56 @@ const classRemover = function (element, value) {
   element.classList.remove(value);
 };
 
-const defaults = function () {
-  boldImp.forEach((op) => {
-    classAdder(op, "opacity-zero");
-  });
-  aboutMeMdi.forEach((mdi) => {
-    classAdder(mdi, "opacity-zero");
-  });
-  classAdder(aboutMePara, "opacity-zero");
-  classAdder(aboutMeImage, "opacity-zero");
-};
-defaults();
-
+// const defaults = function () {
+//   boldImp.forEach((op) => {
+//     classAdder(op, "opacity-zero");
+//   });
+//   aboutMeMdi.forEach((mdi) => {
+//     classAdder(mdi, "opacity-zero");
+//   });
+//   classAdder(aboutMePara, "opacity-zero");
+//   classAdder(aboutMeImage, "opacity-zero");
+// };
+// defaults();
 
 const inputAboutMe = function (entries) {
   const [entry] = entries;
 
-  console.log(entry);
   if (!entry.isIntersecting) return;
 
-  boldImp.forEach((op) => {
-    animationAdder(op, "opacity 1.5s ease-in-out");
+  aboutMeMdi.forEach((mdi) => {
+    animationAdder(mdi, "sideUp 1.5s both");
+    mdi.classList.remove("opacity-zero");
   });
 
-   aboutMeMdi.forEach((mdi) => {
-     animationAdder(mdi, "sideUp 1.5s ease-in-out");
-   });
-
-  animationAdder(aboutMePara, "moveRight 1s ease-in-out");
-  animationAdder(aboutMeImage, "moveRightLeft 1s ease-in-out ");
-  observeAboutMe.unobserve(aboutMe);
+  observeAboutMe.unobserve(aboutMeWork);
 };
 
 const observeAboutMe = new IntersectionObserver(inputAboutMe, {
   root: null,
-  rootMargin: "0px",
-  threshold: 0.2,
+  threshold: 0.1,
 });
-observeAboutMe.observe(aboutMe);
+observeAboutMe.observe( aboutMeWork);
 
+const inputAboutVisual = function (entries) {
+  const [entry] = entries;
 
+  if (!entry.isIntersecting) return;
 
+  boldImp.forEach((op) => {
+    animationAdder(op, "opacity 1.5s ease-in-out");
+    classRemover(op, "opacity-zero");
+  });
+
+  animationAdder(aboutMePara, "moveRight 1s ease-in-out");
+  animationAdder(aboutMeImage, "moveRightLeft 1s ease-in-out ");
+    classRemover(aboutMePara, "opacity-zero");
+    classRemover(aboutMeImage, "opacity-zero");
+  observeAboutMe.unobserve(aboutMeVisual);
+};
+
+const observeAboutVisual = new IntersectionObserver(inputAboutVisual, {
+  root: null,
+  threshold: 0.11,
+});
+observeAboutVisual.observe(aboutMeVisual);
